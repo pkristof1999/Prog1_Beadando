8a0a66cbcd14aa8b45907e97d57eee15022f1137
pkristof1999 <43298291+pkristof1999@users.noreply.github.com>
2020-05-12 16:07:35 +0200

    Update F15_JN8PXT.py

--- F15_JN8PXT.py
+++ F15_JN8PXT.py
@@ -1,11 +1,15 @@
 def rendezes(lst):
-    lst = sorted(lst)
     for i in range(0, len(lst)):
         for j in range(0, len(lst)-i-1):
-            if lst[j][1] < lst[j+1][1]:
+            if lst[j][0] > lst[j+1][0]:
                 tmp = lst[j]
                 lst[j] = lst[j + 1]
                 lst[j+1] = tmp
+            elif lst[j][0] == lst[j+1][0]:
+                if lst[j][1] < lst[j+1][1]:
+                    tmp = lst[j]
+                    lst[j] = lst[j + 1]
+                    lst[j+1] = tmp
 
     for i in range(0, len(lst)):
         print(f"{lst[i]}")
